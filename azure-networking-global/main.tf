63f28c962baf4c80c5ea12721fecdca060507843
Vijayakumar <vijayj292016@gmail.com>
2026-03-11 17:30:25 +0530

    Added version suffix to hub NSG name to avoid existing resource conflict

--- azure-networking-global/main.tf
+++ azure-networking-global/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_subnet" "gateway_subnet" {
 
 # Network Security Group for Hub
 resource "azurerm_network_security_group" "hub_nsg" {
-  name                = "${var.name_prefix}hub-nsg"
+  name                = "${var.name_prefix}hub-nsg-v2"
   location            = azurerm_resource_group.networking_global.location
   resource_group_name = azurerm_resource_group.networking_global.name
   tags                = var.tags
